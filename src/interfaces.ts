c5acc99f4c89a0b8e27359c080643e5e76fe332d
Makeev Ivan <makeev@phystech.edu>
2021-04-13 12:09:02 +0300

    CoinInterface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,21 +7,25 @@ export interface PoolListItemInterface {
     name: string,
 }
 
+export interface CoinInterface {
+    name: string,
+    decimals: number,
+    tethered: boolean,
+    underlying_address: string,
+    wrapped_address?: string,
+    wrapped_decimals?: number,
+}
+
 export interface PoolDataInterface {
     lp_contract: string,
     lp_token_address: string,
     gauge_addresses: string[],
-    coins: ObjectInterface<unknown>[],
+    coins: CoinInterface[],
     swap_address?: string,
     base_pool?: string,
     pool_types?: string[],
     zap_address?: string,
     lp_constructor?: ObjectInterface<unknown>,
     swap_constructor?: ObjectInterface<unknown>,
-    testing?: ObjectInterface<unknown>
-}
-
-export interface BigNumberInterface {
-    _hex: string,
-    _isBigNumber: boolean,
+    testing?: ObjectInterface<unknown>,
 }

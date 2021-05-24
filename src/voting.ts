ffe48bb19a381b4bdbe571f72a9dd373a01340aa
Makeev Ivan <makeev@phystech.edu>
2021-05-24 19:15:40 +0300

    feat: getVeCRV, getVeCRVPct, withdrawLockedCRV

--- src/voting.ts
+++ src/voting.ts
@@ -1,6 +1,8 @@
 import { ethers } from "ethers";
-import { ensureAllowance, getDecimals } from './utils';
+import BigNumber from "bignumber.js";
+import { ensureAllowance, getDecimals, toBN, toStringFromBN } from './utils';
 import { curve, ALIASES } from "./curve";
+import { DictInterface } from "./interfaces";
 
 
 export const getLockedAmountAndUnlockTime = async (address: string): Promise<{ lockedAmount: string, unlockTime: number }> => {
@@ -10,19 +12,61 @@ export const getLockedAmountAndUnlockTime = async (address: string): Promise<{ l
     return { lockedAmount, unlockTime }
 }
 
-export const createLock = async (amount: string, days: number): Promise<string> => {
-    const amountBN = ethers.utils.parseUnits(amount, await getDecimals(ALIASES.crv));
-    const unlockTime = Math.floor(Date.now() / 1000) + (days * 86400);
-    await ensureAllowance([ALIASES.crv], [amountBN], ALIASES.voting_escrow);
+export const getVeCRV = async (...addresses: string[] | string[][]): Promise<DictInterface<string>> => {
+    if (addresses.length == 1 && Array.isArray(addresses[0])) addresses = addresses[0];
+    addresses = addresses as string[];
 
-    return (await curve.contracts[ALIASES.voting_escrow].contract.create_lock(amountBN, unlockTime)).hash
+    const veContract = curve.contracts[ALIASES.voting_escrow].multicallContract;
+    const contractCalls = addresses.map((address: string) => veContract.balanceOf(address));
+    const response: string[] = (await curve.multicallProvider.all(contractCalls)).map((value: ethers.BigNumber) => ethers.utils.formatUnits(value));
+
+    const result: DictInterface<string> = {};
+    addresses.forEach((addr: string, i: number) => {
+        result[addr] = response[i];
+    });
+
+    return result
+}
+
+export const getVeCRVPct = async (...addresses: string[] | string[][]): Promise<DictInterface<string>> => {
+    if (addresses.length == 1 && Array.isArray(addresses[0])) addresses = addresses[0];
+    addresses = addresses as string[];
+
+    const veContract = curve.contracts[ALIASES.voting_escrow].multicallContract;
+    const contractCalls = [veContract.totalSupply()];
+    addresses.forEach((address: string) => {
+        contractCalls.push(veContract.balanceOf(address));
+    });
+    const response: BigNumber[] = (await curve.multicallProvider.all(contractCalls)).map((value: ethers.BigNumber) => toBN(value));
+
+    const [veTotalSupply] = response.splice(0, 1);
+
+    const resultBN: DictInterface<BigNumber> = {};
+    addresses.forEach((acct: string, i: number) => {
+        resultBN[acct] = response[i].div(veTotalSupply).times(100);
+    });
+
+    const result: DictInterface<string> = {};
+    for (const entry of Object.entries(resultBN)) {
+        result[entry[0]] = toStringFromBN(entry[1]);
+    }
+
+    return result
+}
+
+export const createLock = async (amount: string, days: number): Promise<string> => {
+    const _amount = ethers.utils.parseUnits(amount, await getDecimals(ALIASES.crv));
+    const unlockTime = Math.floor(Date.now() / 1000) + (days * 86400);
+    await ensureAllowance([ALIASES.crv], [_amount], ALIASES.voting_escrow);
+
+    return (await curve.contracts[ALIASES.voting_escrow].contract.create_lock(_amount, unlockTime)).hash
 }
 
 export const increaseAmount = async (amount: string): Promise<string> => {
-    const amountBN = ethers.utils.parseUnits(amount, await getDecimals(ALIASES.crv));
-    await ensureAllowance([ALIASES.crv], [amountBN], ALIASES.voting_escrow);
+    const _amount = ethers.utils.parseUnits(amount, await getDecimals(ALIASES.crv));
+    await ensureAllowance([ALIASES.crv], [_amount], ALIASES.voting_escrow);
 
-    return (await curve.contracts[ALIASES.voting_escrow].contract.increase_amount(amountBN)).hash
+    return (await curve.contracts[ALIASES.voting_escrow].contract.increase_amount(_amount)).hash
 }
 
 export const increaseUnlockTime = async (days: number): Promise<string> => {
@@ -31,4 +75,8 @@ export const increaseUnlockTime = async (days: number): Promise<string> => {
     const newUnlockTime = Math.floor(unlockTime / 1000) + (days * 86400);
 
     return (await curve.contracts[ALIASES.voting_escrow].contract.increase_unlock_time(newUnlockTime)).hash
+}
+
+export const withdrawLockedCRV = async (): Promise<string> => {
+    return (await curve.contracts[ALIASES.voting_escrow].contract.withdraw()).hash
 }

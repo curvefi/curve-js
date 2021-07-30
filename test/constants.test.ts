006b0033e7b5c6ffc43dfb85d6b55b210f7725c7
Makeev Ivan <makeev@phystech.edu>
2021-07-30 12:34:20 +0300

    test: checking names and decimals of coins

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,8 +1,16 @@
 import { assert } from "chai";
+import { ethers } from "ethers";
 import { poolsData } from "../src/constants/abis/abis-ethereum";
 import { COINS, DECIMALS } from "../src/constants";
+import { curve } from "../src/curve";
 
 describe('Checking constants', async function () {
+    this.timeout(120000);
+
+    before(async function() {
+        await curve.init('JsonRpc', {}, { gasPrice: 0 });
+    });
+
     it('poolsData <-> COINS match', async function () {
         const COIN_ADDRESSES = Object.values(COINS);
         for (const poolData of Object.values(poolsData)) {
@@ -51,4 +59,43 @@ describe('Checking constants', async function () {
         }
     });
 
-})
+    it('DECIMALS are correct', async function () {
+        for (const [address, decimals] of Object.entries(DECIMALS)) {
+            if (address === "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE") continue; // ETH
+            if (address === "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f") continue; // SNX
+            if (curve.contracts[address] === undefined) console.log(address);
+            const contract = curve.contracts[address].contract;
+            assert.equal(decimals, Number(ethers.utils.formatUnits(await contract.decimals(), 0)), address);
+        }
+    });
+
+    it('poolsData names of coins are correct', async function () {
+        for (const poolData of Object.values(poolsData)) {
+            const coins = [
+                ...poolData.underlying_coins,
+                ...poolData.coins,
+            ]
+            let coinAddresses = [
+                ...poolData.underlying_coin_addresses,
+                ...poolData.coin_addresses,
+            ]
+            if (poolData.is_meta) {
+                coinAddresses = [
+                    poolData.underlying_coin_addresses[0],
+                    ...poolData.meta_coin_addresses as string[],
+                    ...poolData.coin_addresses,
+                ]
+            }
+
+            assert.equal(coins.length, coinAddresses.length, poolData.swap_address);
+            for (let i = 0; i < coins.length; i++) {
+                if (coinAddresses[i] === "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE") continue; // ETH
+                if (coins[i] === "byDAI" || coins[i] === "byUSDC" || coins[i] === "byUSDT") continue; // Actually yDAI, yUSDC, yUSDT
+                if (coins[i] === "sbtcCrv") continue; // Actually crvRenWSBTC
+                if (coins[i] === "ankrETH") continue; // Actually aETHc
+                const contract = curve.contracts[coinAddresses[i]].contract;
+                assert.equal(coins[i], await contract.symbol())
+            }
+        }
+    });
+});

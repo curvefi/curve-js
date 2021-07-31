440a8f6e6aeb0db809aaa94a3c3cc22bd44e28ab
Makeev Ivan <makeev@phystech.edu>
2021-07-31 14:13:49 +0300

    test: full exchange test

--- test/exchange.test.ts
+++ test/exchange.test.ts
@@ -1,20 +1,27 @@
 import { assert } from "chai";
 import { exchangeExpected, exchange } from "../src/pools";
 import { BN, getBalances } from "../src/utils";
+import { COINS } from "../src/constants/coins";
 import { curve } from "../src/curve";
 
-const bestExchangeTest = async (coin1: string, coin2: string) => {
+const exchangeTest = async (coin1: string, coin2: string) => {
     const address = curve.signerAddress;
-    const amount = '100';
+    const amount = '1';
     const initialBalances = (await getBalances([address], [coin1, coin2]))[address];
 
     const output = await exchangeExpected(coin1, coin2, amount);
     await exchange(coin1, coin2, amount);
 
-    const balancesAfterSwap = (await getBalances([address], [coin1, coin2]))[address];
+    const balances = (await getBalances([address], [coin1, coin2]))[address];
 
-    assert.deepStrictEqual(BN(balancesAfterSwap[0]), BN(initialBalances[0]).minus(BN(amount)));
-    assert.isAtLeast(Number(balancesAfterSwap[1]), Number(BN(initialBalances[1]).plus(BN(output).times(0.99)).toString()));
+    if (coin1 === 'steth' || coin2 === 'steth') {
+        assert.approximately(Number(Object.values(balances)[0]), Number(BN(Object.values(initialBalances)[0]).minus(BN(amount)).toString()), 1e-18);
+    } else if (['adai', 'ausdc', 'ausdt', 'asusd'].includes(coin1) || ['adai', 'ausdc', 'ausdt', 'asusd'].includes(coin2)) {
+        assert.approximately(Number(Object.values(balances)[0]), Number(BN(Object.values(initialBalances)[0]).minus(BN(amount)).toString()), 1e-4);
+    } else {
+        assert.deepStrictEqual(BN(balances[0]), BN(initialBalances[0]).minus(BN(amount)));
+    }
+    assert.isAtLeast(Number(balances[1]), Number(BN(initialBalances[1]).plus(BN(output).times(0.99)).toString()));
 }
 
 describe('Exchange using all pools', async function () {
@@ -24,26 +31,20 @@ describe('Exchange using all pools', async function () {
         await curve.init('JsonRpc', {}, { gasPrice: 0 });
     });
 
-    const DAI = "0x6b175474e89094c44da98b954eedeac495271d0f";
-    const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-    const cDAI = "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643";
-    const cUSDC = "0x39AA39c021dfbaE8faC545936693aC917d5E7563";
-
-    it('DAI --> USDC (underlying)', async function () {
-        await bestExchangeTest(DAI, USDC)
-    });
-
-    it('cDAI --> cUSDC (wrapped)', async function () {
-        await bestExchangeTest(cDAI, cUSDC)
-    });
-
-    it('DAI --> cUSDC (error)', async function () {
-        const amount = '100';
-
-        try {
-            await exchange(DAI, cUSDC, amount);
-        } catch (err) {
-            assert.equal(err.message, "This pair can't be exchanged")
+    for (const coin1 of Object.keys(COINS).slice(0, 4)) {
+        for (const coin2 of Object.keys(COINS)) {
+            if (coin1 === 'snx' || coin2 === 'snx') continue;
+            if (coin1 === 'eurs' || coin2 === 'eurs') continue; // TODO remove
+            if (coin1 !== coin2) {
+                it(`${coin1} --> ${coin2}`, async function () {
+                    try {
+                        await exchangeTest(coin1, coin2);
+                    } catch (err) {
+                        console.log(err.message);
+                        assert.equal(err.message, "This pair can't be exchanged")
+                    }
+                });
+            }
         }
-    });
+    }
 })

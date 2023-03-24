0765c0e551521735de616967fc4d72467527f6b5
macket <macket24@proton.me>
2023-03-24 11:15:59 +0400

    test: factoryPoolsData

---
+++ test/factoryPoolsData.test.ts
@@ -0,0 +1,94 @@
+import { assert } from "chai";
+import { curve } from "../src/curve";
+import { ETH_RPC } from "./rpcUrls.test";
+import { IDict, IPoolData } from "../src/interfaces";
+
+
+const factoryPoolsDataTest = (factoryPoolsDataFromApi: IDict<IPoolData>, factoryPoolsData: IDict<IPoolData>, isStrict: boolean) => {
+    assert.deepStrictEqual(Object.keys(factoryPoolsDataFromApi), Object.keys(factoryPoolsData));
+
+    for (const poolId in factoryPoolsData) {
+        assert.equal(factoryPoolsData[poolId].name, factoryPoolsDataFromApi[poolId].name, `${poolId}: name`);
+        assert.equal(factoryPoolsData[poolId].full_name, factoryPoolsDataFromApi[poolId].full_name, `${poolId}: full_name`);
+        assert.equal(factoryPoolsData[poolId].symbol, factoryPoolsDataFromApi[poolId].symbol, `${poolId}: symbol`);
+
+        if (isStrict) {
+            assert.equal(factoryPoolsData[poolId].reference_asset, factoryPoolsDataFromApi[poolId].reference_asset, `${poolId}: reference_asset`);
+        } else {
+            if (factoryPoolsData[poolId].reference_asset !== factoryPoolsDataFromApi[poolId].reference_asset) {
+                console.log(poolId, `reference asset: ${factoryPoolsData[poolId].reference_asset} (blockchain) != ${factoryPoolsDataFromApi[poolId].reference_asset} (api)`);
+                console.log("Wrapped coins:", factoryPoolsDataFromApi[poolId].wrapped_coins);
+                console.log("Underlying coins:", factoryPoolsDataFromApi[poolId].underlying_coins, "\n");
+            }
+        }
+
+        assert.equal(factoryPoolsData[poolId].swap_address, factoryPoolsDataFromApi[poolId].swap_address, `${poolId}: swap_address`);
+        assert.equal(factoryPoolsData[poolId].token_address, factoryPoolsDataFromApi[poolId].token_address, `${poolId}: token_address`);
+        assert.equal(factoryPoolsData[poolId].gauge_address, factoryPoolsDataFromApi[poolId].gauge_address, `${poolId}: gauge_address`);
+        assert.equal(factoryPoolsData[poolId].deposit_address, factoryPoolsDataFromApi[poolId].deposit_address, `${poolId}: deposit_address`);
+        assert.equal(factoryPoolsData[poolId].sCurveRewards_address, factoryPoolsDataFromApi[poolId].sCurveRewards_address, `${poolId}: sCurveRewards_address`);
+        assert.equal(factoryPoolsData[poolId].reward_contract, factoryPoolsDataFromApi[poolId].reward_contract, `${poolId}: reward_contract`);
+        assert.equal(factoryPoolsData[poolId].implementation_address, factoryPoolsDataFromApi[poolId].implementation_address, `${poolId}: implementation_address`);
+        assert.equal(factoryPoolsData[poolId].is_plain, factoryPoolsDataFromApi[poolId].is_plain, `${poolId}: is_plain`);
+        assert.equal(factoryPoolsData[poolId].is_lending, factoryPoolsDataFromApi[poolId].is_lending, `${poolId}: is_lending`);
+        assert.equal(factoryPoolsData[poolId].is_meta, factoryPoolsDataFromApi[poolId].is_meta, `${poolId}: is_meta`);
+        assert.equal(factoryPoolsData[poolId].is_crypto, factoryPoolsDataFromApi[poolId].is_crypto, `${poolId}: is_crypto`);
+        assert.equal(factoryPoolsData[poolId].is_fake, factoryPoolsDataFromApi[poolId].is_fake, `${poolId}: is_fake`);
+        assert.equal(factoryPoolsData[poolId].is_factory, factoryPoolsDataFromApi[poolId].is_factory, `${poolId}: is_factory`);
+        assert.equal(factoryPoolsData[poolId].base_pool, factoryPoolsDataFromApi[poolId].base_pool, `${poolId}: base_pool`);
+
+        if (isStrict) {
+            assert.deepStrictEqual(factoryPoolsData[poolId].underlying_coins, factoryPoolsDataFromApi[poolId].underlying_coins, `${poolId}: underlying_coins`);
+        } else {
+            if (JSON.stringify(factoryPoolsData[poolId].underlying_coins) !== JSON.stringify(factoryPoolsDataFromApi[poolId].underlying_coins)) {
+                console.log(poolId, `underlying_coins: ${factoryPoolsData[poolId].underlying_coins} (blockchain) != ${factoryPoolsDataFromApi[poolId].underlying_coins} (api) \n`);
+            }
+        }
+
+        if (isStrict) {
+            assert.deepStrictEqual(factoryPoolsData[poolId].wrapped_coins, factoryPoolsDataFromApi[poolId].wrapped_coins, `${poolId}: wrapped_coins`);
+        } else {
+            if (JSON.stringify(factoryPoolsData[poolId].wrapped_coins) !== JSON.stringify(factoryPoolsDataFromApi[poolId].wrapped_coins)) {
+                console.log(poolId, `wrapped_coins: ${factoryPoolsData[poolId].wrapped_coins} (blockchain) != ${factoryPoolsDataFromApi[poolId].wrapped_coins} (api) \n`);
+            }
+        }
+
+        assert.deepStrictEqual(factoryPoolsData[poolId].underlying_coin_addresses, factoryPoolsDataFromApi[poolId].underlying_coin_addresses, `${poolId}: underlying_coin_addresses`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].wrapped_coin_addresses, factoryPoolsDataFromApi[poolId].wrapped_coin_addresses, `${poolId}: wrapped_coin_addresses`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].underlying_decimals, factoryPoolsDataFromApi[poolId].underlying_decimals, `${poolId}: underlying_decimals`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].wrapped_decimals, factoryPoolsDataFromApi[poolId].wrapped_decimals, `${poolId}: wrapped_decimals`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].use_lending, factoryPoolsDataFromApi[poolId].use_lending, `${poolId}: use_lending`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].swap_abi, factoryPoolsDataFromApi[poolId].swap_abi, `${poolId}: swap_abi`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].gauge_abi, factoryPoolsDataFromApi[poolId].gauge_abi, `${poolId}: gauge_abi`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].deposit_abi, factoryPoolsDataFromApi[poolId].deposit_abi, `${poolId}: deposit_abi`);
+        assert.deepStrictEqual(factoryPoolsData[poolId].sCurveRewards_abi, factoryPoolsDataFromApi[poolId].sCurveRewards_abi, `${poolId}: sCurveRewards_abi`);
+    }
+}
+
+describe('Factory pools data', async function () {
+    this.timeout(120000);
+
+    before(async function() {
+        await curve.init('JsonRpc', { url: ETH_RPC }, { gasPrice: 0 });
+        // await curve.fetchCryptoFactoryPools();
+    });
+
+    it('Factory', async function () {
+        await curve.fetchFactoryPools();
+        const factoryPoolsDataFromApi = { ...curve.constants.FACTORY_POOLS_DATA };
+        delete factoryPoolsDataFromApi['factory-v2-234'];
+        await curve.fetchFactoryPools(false);
+        const factoryPoolsData = { ...curve.constants.FACTORY_POOLS_DATA };
+
+        factoryPoolsDataTest(factoryPoolsDataFromApi, factoryPoolsData, false);
+    });
+
+    it('Crypto factory', async function () {
+        await curve.fetchFactoryPools();
+        const cryptoFactoryPoolsDataFromApi = { ...curve.constants.CRYPTO_FACTORY_POOLS_DATA };
+        await curve.fetchFactoryPools(false);
+        const cryptoFactoryPoolsData = { ...curve.constants.CRYPTO_FACTORY_POOLS_DATA };
+
+        factoryPoolsDataTest(cryptoFactoryPoolsDataFromApi, cryptoFactoryPoolsData, true);
+    });
+});

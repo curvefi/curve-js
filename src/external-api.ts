32850934b115860404997baaa82415133df8fbd4
macket <macket24@proton.me>
2023-06-03 22:57:00 +0400

    fix: rewards apy for tricrypto factory (useApi=true)

--- src/external-api.ts
+++ src/external-api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import memoize from "memoizee";
-import { IExtendedPoolDataFromApi, ISubgraphPoolData, IDict, INetworkName } from "./interfaces";
+import { IExtendedPoolDataFromApi, ISubgraphPoolData, IDict, INetworkName, IPoolType } from "./interfaces";
 
 
 export const _getPoolsFromApi = memoize(
-    async (network: INetworkName, poolType: "main" | "crypto" | "factory" | "factory-crvusd" | "factory-crypto" | "factory-tricrypto"): Promise<IExtendedPoolDataFromApi> => {
+    async (network: INetworkName, poolType: IPoolType): Promise<IExtendedPoolDataFromApi> => {
         const url = `https://api.curve.fi/api/getPools/${network}/${poolType}`;
         const response = await axios.get(url, { validateStatus: () => true });
         return response.data.data ?? { poolData: [], tvl: 0, tvlAll: 0 };
@@ -22,6 +22,7 @@ export const _getAllPoolsFromApi = async (network: INetworkName): Promise<IExten
         _getPoolsFromApi(network, "factory"),
         _getPoolsFromApi(network, "factory-crvusd"),
         _getPoolsFromApi(network, "factory-crypto"),
+        _getPoolsFromApi(network, "factory-tricrypto"),
     ]);
 }
 

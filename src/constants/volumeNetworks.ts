4c2e73491a30698c06cd8a51294cb8c270e60e73
fedorovdg <fedorovdgap@gmail.com>
2025-01-17 16:28:05 +0400

    chore: change volume methods for 10,100,250 networks

--- src/constants/volumeNetworks.ts
+++ src/constants/volumeNetworks.ts
@@ -7,7 +7,7 @@ export interface IVolumeNetworks {
 }
 
 export const volumeNetworks: IVolumeNetworks = {
-    getVolumes: [1,137,252,8453,42161],
-    getSubgraphData: [10,100,250,43114],
+    getVolumes: [1,10,100,137,250,252,8453,42161],
+    getSubgraphData: [43114],
     getFactoryAPYs: [56,196,324,1284,2222,5000,42220,1313161554],
 }

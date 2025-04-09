0b110a76055a31ceecb4930326426a07a931c17d
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-04-09 12:51:57 +0200

    fix: review comments

--- src/cached.ts
+++ src/cached.ts
@@ -1,5 +1,5 @@
 import {IDict, IExtendedPoolDataFromApi, INetworkName, IPoolType} from "./interfaces.js";
-import {uncached_getAllPoolsFromApi, createCrvApyDict, createUsdPricesDict} from './external-api.js'
+import {createCrvApyDict, createUsdPricesDict, uncached_getAllPoolsFromApi} from './external-api.js'
 import {curve} from "./curve";
 
 /**
@@ -7,49 +7,75 @@ import {curve} from "./curve";
  * Custom function instead of `memoizee` because we want to be able to set the cache manually based on server data.
  * @param fn The function that returns a promise and will be memoized
  * @param maxAge The maximum age of the cache in milliseconds
+ * @param createKey A function that creates a key for the cache based on the arguments passed to the function
  * @returns A memoized `fn` function that includes a `set` method to set the cache manually
  */
-const memoize = <TResult, TParams extends any[], TFunc extends (...args: TParams) => Promise<TResult>>(fn: TFunc, {maxAge}: {
-    maxAge: number
+const memoize = <TResult, TParams extends any[], TFunc extends (...args: TParams) => Promise<TResult>>(fn: TFunc, {
+    maxAge,
+    createKey = (list) => list.toString(),
+}: {
+    maxAge: number,
+    createKey?: (args: TParams) => string
 }) => {
     const cache: Record<string, Promise<TResult>> = {};
+    const timeouts: Record<string, ReturnType<typeof setTimeout>> = {};
+
+    const setCache = (key: string, promise?: Promise<TResult>) => {
+        if (promise) {
+            cache[key] = promise;
+        } else if (key in cache) {
+            delete cache[key];
+        }
+        if (key in timeouts) {
+            clearTimeout(timeouts[key]);
+            delete timeouts[key]
+        }
+    };
+
+    const scheduleCleanup = (key: string) => timeouts[key] = setTimeout(() => {
+        delete timeouts[key];
+        delete cache[key];
+    }, maxAge);
+
     const cachedFn = async (...args: TParams): Promise<TResult> => {
-        const key = JSON.stringify(args);
+        const key = createKey(args);
         if (key in cache) {
             return cache[key];
         }
         const promise = fn(...args);
-        cache[key] = promise;
+        setCache(key, promise);
         try {
             const result = await promise;
-            setTimeout(() => delete cache[key], maxAge);
+            scheduleCleanup(key)
             return result;
         } catch (e) {
             delete cache[key];
             throw e;
         }
     };
+
     cachedFn.set = (result: TResult, ...args: TParams) => {
-        const key = JSON.stringify(args);
-        setTimeout(() => delete cache[key], maxAge);
-        cache[key] = Promise.resolve(result);
+        const key = createKey(args);
+        setCache(key, Promise.resolve(result));
+        scheduleCleanup(key);
     }
+
     return cachedFn as TFunc & { set: (result: TResult, ...args: TParams) => void };
 }
 
-function createCache(poolsDict: Record<"main" | "crypto" | "factory" | "factory-crvusd" | "factory-eywa" | "factory-crypto" | "factory-twocrypto" | "factory-tricrypto" | "factory-stable-ng", IExtendedPoolDataFromApi>) {
+const createCache = (poolsDict: Record<IPoolType, IExtendedPoolDataFromApi>) => {
     const poolLists = Object.values(poolsDict)
     const usdPrices = createUsdPricesDict(poolLists);
     const crvApy = createCrvApyDict(poolLists)
     return {poolsDict, poolLists, usdPrices, crvApy};
-}
+};
 
 /**
  * This function is used to cache the data fetched from the API and the data derived from it.
  * Note: do not expose this function to the outside world, instead encapsulate it in a function that returns the data you need.
  */
 const _getCachedData = memoize(async (network: INetworkName, isLiteChain: boolean) =>
-    createCache(await uncached_getAllPoolsFromApi(network, isLiteChain)), { maxAge: 1000 * 60 * 5 /* 5 minutes */ })
+    createCache(await uncached_getAllPoolsFromApi(network, isLiteChain)), {maxAge: 1000 * 60 * 5 /* 5 minutes */})
 
 export const _getPoolsFromApi =
     async (network: INetworkName, poolType: IPoolType, isLiteChain = false): Promise<IExtendedPoolDataFromApi> => {
@@ -59,11 +85,7 @@ export const _getPoolsFromApi =
 
 export const _setPoolsFromApi =
     (network: INetworkName, isLiteChain: boolean, data: Record<IPoolType, IExtendedPoolDataFromApi>): void =>
-        _getCachedData.set(
-            createCache(data),
-            network,
-            isLiteChain
-        )
+        _getCachedData.set(createCache(data), network, isLiteChain)
 
 export const _getAllPoolsFromApi = async (network: INetworkName, isLiteChain = false): Promise<IExtendedPoolDataFromApi[]> => {
     const {poolLists} = await _getCachedData(network, isLiteChain);

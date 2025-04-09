51ce8b52d63e7f0249c676555c8420b30d912279
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-04-09 10:54:00 +0200

    feat: allow pool data injection

--- src/cached.ts
+++ src/cached.ts
@@ -1,28 +1,61 @@
-import memoize from "memoizee";
 import {IDict, IExtendedPoolDataFromApi, INetworkName, IPoolType} from "./interfaces.js";
 import {uncached_getAllPoolsFromApi, uncached_getCrvApyFromApi, uncached_getUsdPricesFromApi} from './external-api.js'
 import {curve} from "./curve";
 
-const _getCachedData = memoize(
-    async (network: INetworkName, isLiteChain: boolean) => {
-        const poolsDict = await uncached_getAllPoolsFromApi(network, isLiteChain);
-        const poolLists = Object.values(poolsDict)
-        const usdPrices = uncached_getUsdPricesFromApi(poolLists);
-        const crvApy = uncached_getCrvApyFromApi(poolLists)
-        return { poolsDict, poolLists, usdPrices, crvApy };
-    },
-    {
-        promise: true,
-        maxAge: 5 * 60 * 1000, // 5m
-        primitive: true,
+const memoize = <TResult, TParams extends any[], TFunc extends (...args: TParams) => Promise<TResult>>(fn: TFunc, {maxAge}: {
+    maxAge: number
+}) => {
+    const cache: Record<string, Promise<TResult>> = {};
+    const cachedFn = async (...args: TParams): Promise<TResult> => {
+        const key = JSON.stringify(args);
+        if (key in cache) {
+            return cache[key];
+        }
+        const promise = fn(...args);
+        cache[key] = promise;
+        try {
+            const result = await promise;
+            setTimeout(() => delete cache[key], maxAge);
+            return result;
+        } catch (e) {
+            delete cache[key];
+            throw e;
+        }
+    };
+    cachedFn.set = (result: TResult, ...args: TParams) => {
+        const key = JSON.stringify(args);
+        setTimeout(() => delete cache[key], maxAge);
+        cache[key] = Promise.resolve(result);
     }
-)
+    return cachedFn as TFunc & { set: (result: TResult, ...args: TParams) => void };
+}
+
+function createCache(poolsDict: Record<"main" | "crypto" | "factory" | "factory-crvusd" | "factory-eywa" | "factory-crypto" | "factory-twocrypto" | "factory-tricrypto" | "factory-stable-ng", IExtendedPoolDataFromApi>) {
+    const poolLists = Object.values(poolsDict)
+    const usdPrices = uncached_getUsdPricesFromApi(poolLists);
+    const crvApy = uncached_getCrvApyFromApi(poolLists)
+    return {poolsDict, poolLists, usdPrices, crvApy};
+}
+
+const _getCachedData = memoize(
+    async (network: INetworkName, isLiteChain: boolean) => createCache(await uncached_getAllPoolsFromApi(network, isLiteChain))
+    , {
+        maxAge: 1000 * 60 * 5, // 5 minutes
+    })
 
 export const _getPoolsFromApi =
     async (network: INetworkName, poolType: IPoolType, isLiteChain = false): Promise<IExtendedPoolDataFromApi> => {
         const {poolsDict} = await _getCachedData(network, isLiteChain);
         return poolsDict[poolType]
     }
+
+export const _setPoolsFromApi =
+    (network: INetworkName, isLiteChain: boolean, data: Record<IPoolType, IExtendedPoolDataFromApi>): void =>
+        _getCachedData.set(
+            createCache(data),
+            network,
+            isLiteChain
+        )
 
 export const _getAllPoolsFromApi = async (network: INetworkName, isLiteChain = false): Promise<IExtendedPoolDataFromApi[]> => {
     const {poolLists} = await _getCachedData(network, isLiteChain);

ba2392bf641e6b61735608ecb5e6cc3a8bc7b311
fedorovdg <fedorovdgap@gmail.com>
2026-08-06 15:11:46 +0300

    fix: return Fantom

--- src/constants/pools/index.ts
+++ src/constants/pools/index.ts
@@ -1,6 +1,7 @@
 import { POOLS_DATA_ETHEREUM } from "./ethereum.js";
 import { LLAMMAS_DATA_ETHEREUM } from "./ethereum.js";
 import { POOLS_DATA_POLYGON } from "./polygon.js";
+import { POOLS_DATA_FANTOM } from "./fantom.js";
 import { POOLS_DATA_AVALANCHE } from "./avalanche.js";
 import { POOLS_DATA_ARBITRUM } from "./arbitrum.js";
 import { POOLS_DATA_OPTIMISM } from "./optimism.js";
@@ -21,6 +22,7 @@ export {
     POOLS_DATA_ETHEREUM,
     LLAMMAS_DATA_ETHEREUM,
     POOLS_DATA_POLYGON,
+    POOLS_DATA_FANTOM,
     POOLS_DATA_AVALANCHE,
     POOLS_DATA_ARBITRUM,
     POOLS_DATA_OPTIMISM,
